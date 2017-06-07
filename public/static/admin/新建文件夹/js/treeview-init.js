9196659cdbf26850304970402fa4f439b80d9c02
longtengzlq <286243884@qq.com>
2017-06-08 06:58:39 +0800

    upload static/admin,static/index

---
+++ public/static/admin/新建文件夹/js/treeview-init.js
@@ -0,0 +1,52 @@
+﻿
+var UITree = function () {
+
+    return {
+        //main function to initiate the module
+        init: function (sdata) {
+		//	alert(sdata);
+            var DataSourceTree = function (options) {
+                this._data = options.data;
+                this._delay = options.delay;
+            };
+
+            DataSourceTree.prototype = {
+
+                data: function (options, callback) {
+                    var self = this;
+
+                    setTimeout(function () {
+                        var data = $.extend(true, [], self._data);
+
+                        callback({ data: data });
+
+                    }, this._delay)
+                }
+            };
+
+            // INITIALIZING TREE
+			temp=sdata.substring(0,sdata.length);
+
+			var treeDataSource2 = new DataSourceTree({
+				
+                data: JSON.parse(temp),
+				delay: 400
+            });
+
+
+
+            $('#MyTree2').tree({
+                dataSource: treeDataSource2,
+                multiSelect: true,
+		'open-icon': 'ace-icon tree-minus',
+                'close-icon': 'ace-icon tree-plus',
+                'selectable': false,
+                loadingHTML: '<div class="tree-loading"><i class="fa fa-rotate-right fa-spin"></i></div>'
+            });
+
+          
+        }
+
+    };
+
+}();
