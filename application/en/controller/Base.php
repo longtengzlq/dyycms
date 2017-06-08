1bd274cc07fda64d5e07b5eb5b93f03c9b3ddefb
longtengzlq <286243884@qq.com>
2017-06-08 11:10:22 +0800

    将英文栏目也修改完毕，基本可以放到网上运行了

--- application/en/controller/Base.php
+++ application/en/controller/Base.php
@@ -17,9 +17,8 @@ use think\Request;
  * @author ZLQ
  */
 class Base  extends Controller{
-    function _initialize() {
-        parent::_initialize();
-         //取出栏目信息，所有前台页面均需用到故放到Base类中去除
+     function _initialize() {
+        //取出栏目信息，所有前台页面均需用到故放到Base类中去除
         try {
             $cates = db('category')->field('id,cate_name,pid,type,model_type_id')->where(array('status' => 1, 'language_id' => 2, 'pid' => 0))->select();
         } catch (\Exception $exc) {
@@ -30,16 +29,19 @@ class Base  extends Controller{
         }
 
         //热门文章推荐
-        $hot_arts= Article::name('article')->where(array('language_id'=>'1','status'=>1))->where('thumb', 'neq', '')->order('zan desc')->limit(5)->select();
+        $hot_arts= Article::name('article')->where(array('language_id'=>2,'status'=>1))->where('thumb', 'neq', '')->order('zan desc')->limit(5)->select();
+
          $this->assign('hot_arts',$hot_arts);
         //对顶级栏目进行排序增加其子元素
         $this->sortCates($cates);        
         //dump($cates);
         $this->assign('sort_cates',$cates);
+        parent::_initialize();
        
     }
     protected function sortCates(&$cates){
         foreach ($cates as $key => $cate) {
+            //这里是个性能瓶颈，多次去数据库中取数据，可以利用两层foreach实现
             $children=db('category')->where('pid',$cate['id'] )->select();
             if(count($children)!=0){
                 $cates[$key]['children']=$children;            
